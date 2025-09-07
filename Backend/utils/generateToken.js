696a1cc50d73fe0baa46d692a8cffc4f54cd9ea9
Subhajit Ghosh <gamingsubha07@gmail.com>
2025-09-08 01:45:03 +0530

    Update generateToken.js
    change cookie part

--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -1,14 +1,16 @@
 import jwt from "jsonwebtoken";
 
 const generateTokenAndSetCookie = (userId, res) => {
-	const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-		expiresIn: "15d",
-	});
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+        expiresIn: "15d",
+    });
 
-	res.cookie("token", token, {
-		maxAge: 15 * 24 * 60 * 60 * 1000, 
-		httpOnly: true, 
-	});
+    res.cookie("token", token, {
+        maxAge: 15 * 24 * 60 * 60 * 1000, 
+        httpOnly: true,
+        sameSite: 'none',
+        secure: true,
+    });
 };
 
 export default generateTokenAndSetCookie;
