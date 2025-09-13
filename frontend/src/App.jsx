c66762a85d6cbc44125e510360a1cdf0427a29c8
Subhajit Ghosh <gamingsubha07@gmail.com>
2025-09-14 00:33:09 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,30 +16,37 @@ import Register from './pages/Register';
 import { useContext } from 'react';
 import { UserInfo } from './context/AuthContext';
 
-
 function App() {
-  const{authUser, setAuthUser, loading} = useContext(UserInfo);
-  
-  return (
-      <Routes>
-          <Route path="/" element={<Navbar />}>
-                <Route index element={<Navigate to="/home" replace />} />
-                <Route path="home" element={<Home />} />
-                <Route path="dashboard" element={authUser ? <Dashboard /> : <Navigate to="/" />} />
-                <Route path="products_admin" element={authUser ? <Products_Admin /> : <Navigate to="/" />} />
-                <Route path="products_cus" element={authUser ? <Products_Cus /> : <Navigate to="/" />} />
-                <Route path="categories" element={authUser ? <Categories /> : <Navigate to="/" />} />
-                <Route path="order_admin" element={authUser ? <Orders_Admin /> : <Navigate to="/" />} />
-                <Route path="order_cus" element={authUser ? <Orders_Cus /> : <Navigate to="/" />} />
-                <Route path="suppliers" element={authUser ? <Suppliers /> : <Navigate to="/" />} />
-                <Route path="users" element={authUser ? <Users /> : <Navigate to="/" />} />
-                <Route path="profile" element={authUser ? <Profile /> : <Navigate to="/" />} />
-          </Route>
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/register" element={<Register />} />
-      </Routes>
+  const { authUser, loading } = useContext(UserInfo);
 
-  )
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="w-16 h-16 border-4 border-blue-500 border-dashed rounded-full animate-spin"></div>
+      </div>
+    );
+  }
+
+  return (
+    <Routes>
+      <Route path="/" element={<Navbar />}>
+        <Route index element={<Navigate to="/home" replace />} />
+        <Route path="home" element={<Home />} />
+        <Route path="dashboard" element={authUser ? <Dashboard /> : <Navigate to="/" />} />
+        <Route path="products_admin" element={authUser ? <Products_Admin /> : <Navigate to="/" />} />
+        <Route path="products_cus" element={authUser ? <Products_Cus /> : <Navigate to="/" />} />
+        <Route path="categories" element={authUser ? <Categories /> : <Navigate to="/" />} />
+        <Route path="order_admin" element={authUser ? <Orders_Admin /> : <Navigate to="/" />} />
+        <Route path="order_cus" element={authUser ? <Orders_Cus /> : <Navigate to="/" />} />
+        <Route path="suppliers" element={authUser ? <Suppliers /> : <Navigate to="/" />} />
+        <Route path="users" element={authUser ? <Users /> : <Navigate to="/" />} />
+        <Route path="profile" element={authUser ? <Profile /> : <Navigate to="/" />} />
+      </Route>
+
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
